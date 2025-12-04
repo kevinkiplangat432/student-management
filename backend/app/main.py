ad5edf5c51bac153cfb4ca8c8b17f8e88891a2dc
kevin kiplangat <kiplangatkevin335@gmail.com>
2025-12-04 22:13:24 +0300

    main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,14 @@
+# in this file  i import fast Api and route modules.
+# routes modules are impported to registyer them with app.
+
+
 from fastapi import FastAPI
 from app.api.routes import auth, users, courses,  enrollments, students
 
-app = FastAPI(title="Student Management System API")
+#handle http requests.
+app = FastAPI(title="Student Management System API") # create a Fast APIinstance
 
-
+#register route modules with the app.
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(users.router, prefix="/users", tags=["Users"]) 
 app.include_router(enrollments.router, prefix="/enrollments", tags=["Enrollments"])
@@ -11,3 +16,10 @@ app.include_router(enrollments.router, prefix="/enrollments", tags=["Enrollments
 @app.get("/")
 def root():
     return {"message": "Student Management Backend Running"}
+
+#study:
+# include_router() connect each module's route to the main app.
+# app.include_router
+# (auth.router
+# , prefix="/auth", this means all routes in auth.router will start with /auth 
+# tags=["Auth"]) this syntax groups these endpoints in the auto generated APi docs
